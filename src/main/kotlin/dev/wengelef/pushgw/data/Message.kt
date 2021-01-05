ed455ae3d1514a71728fcb81f3a24f99b6348646
Flo <florian.wengelewski@rewe-digital.com>
2021-01-05 13:38:35 +0100

    closes #9: add android push config

--- src/main/kotlin/dev/wengelef/pushgw/data/Message.kt
+++ src/main/kotlin/dev/wengelef/pushgw/data/Message.kt
@@ -1,5 +1,6 @@
 package dev.wengelef.pushgw.data
 
+import dev.wengelef.pushgw.data.android.AndroidPushConfig
 import dev.wengelef.pushgw.data.apple.ApplePushConfig
 import dev.wengelef.pushgw.data.web.WebPushConfig
 import kotlinx.serialization.Serializable
@@ -11,7 +12,8 @@ data class Message(
     val notification: Notification? = null,
     val topic: String? = null,
     val webpush: WebPushConfig? = null,
-    val applePushConfig: ApplePushConfig? = null
+    val applePushConfig: ApplePushConfig? = null,
+    val androidPushConfig: AndroidPushConfig? = null
 )
 
 @Serializable
